0b251788ba7ac35a00740940d9113d5b26fc306a
Yeongjun Cho <71057287+cyjunee@users.noreply.github.com>
2023-02-16 18:59:36 +0900

    [Gold III] Title: 텀 프로젝트, Time: 2296 ms, Memory: 59208 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -1,22 +1,28 @@
-from sys import stdin
+import sys
+sys.setrecursionlimit(100100)
+
+def DFS(start):
+    path.append(start)
+    visit[start] = True
+    next = graph[start]
+    
+    if not visit[next]:
+        return DFS(next)
+    elif next in path:
+        return len(path) - path.index(next)
+
+    return 0
 
 T = int(input())
 for _ in range(T):
     N = int(input())
-    arr = [0] + list(map(int, stdin.readline().split()))
-    visit = [False] * (N + 1)
-    result = 0
+    graph = [0] + list(map(int, input().split()))
+    visit = [False] * (N+1)
+    node_in_cycle = 0
     
-    for i in range(1, N+1):
-        path = []
-        cur = i
-
-        while not visit[cur]:
-            visit[cur] = True
-            path.append(cur)
-            cur = arr[cur]
+    for i in range(1,N+1):
+        if not visit[i]:
+            path = []
+            node_in_cycle += DFS(i)
     
-        if cur in path:
-            result += len(path) - path.index(cur)
-
-    print(N - result)
+    print(N - node_in_cycle)
